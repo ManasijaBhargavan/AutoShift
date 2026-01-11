bdca25a4ab6013e04201754aa219487ec71f5042
ManasijaBhargavan <manas@ucsb.edu>
2026-01-10 23:40:57 -0800

    change hardcoded url

--- src/App.jsx
+++ src/App.jsx
@@ -248,7 +248,7 @@ const App = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:3001/api/save-availability', {
+      const response = await fetch('/api/save-availability', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(finalData),
