9cc59a53599e1b97c92822fee586bb4ecee5a35e
ManasijaBhargavan <manas@ucsb.edu>
2026-01-11 00:00:01 -0800

    remove localhost

--- src/Employer.jsx
+++ src/Employer.jsx
@@ -182,7 +182,7 @@ const Employer = () => {
       // You can increase this delay if your python script is very slow
       setTimeout(async () => {
         try {
-          const schedRes = await fetch('http://localhost:3001/api/schedule');
+          const schedRes = await fetch('/api/schedule');
           const schedData = await schedRes.json();
           
           if (schedData.schedule) {
